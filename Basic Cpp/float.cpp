5956de4d10dbe08606743e53545b476096783410
AskatAsh <abdulaskat@gmail.com>
2023-07-24 20:11:58 +0600

    first commit

---
+++ Basic Cpp/float.cpp
@@ -0,0 +1,10 @@
+// #include<iostream>
+// #include<iomanip>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // float x= 34.248590;
+    double x= 34.248590;
+    // cout<<x<<endl;
+    cout<<fixed<<setprecision(6)<<x<<endl;
+}
