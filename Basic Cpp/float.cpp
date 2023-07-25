b1daf1c385a6c9ea28cbe83a4153ee91eafc2c99
AskatAsh <abdulaskat@gmail.com>
2023-07-25 23:17:33 +0600

    separating various input output using comments

--- Basic Cpp/float.cpp
+++ Basic Cpp/float.cpp
@@ -3,20 +3,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    // // float x= 34.248590;
-    // double x= 34.248590;
-    // // cout<<x<<endl;
-    // cout<<fixed<<setprecision(6)<<x<<endl;
-    // char name[50];
-    // //cin>>name; // this takes input without spaces
-    // cin.getline(name,50); // this takes input with spaces
-    // cout<<name<<endl;
+// datatype input and output:
+    int a = 66;
+    float x= 34.248590;
+    double y= 98.87664932815;
+    cin>>a>>x>>y;
+    cout<<a<<" "<<fixed<<setprecision(5)<<x<<" "<<y<<endl;
 
-    int num[5];
-    for(int i=0;i<5;i++){
+// string input and output:
+    char name[50];
+    //cin>>name; // this takes input without spaces
+    cin.getline(name,50); // this takes input with spaces
+    cout<<name<<endl;
+
+// intger array input and output:
+    int num[5],i;
+    for(i=0;i<5;i++){
         cin>>num[i];
     }
-    for(int i=0;i<5;i++){
+    for(i=0;i<5;i++){
         cout<<num[i]<<" ";
     }
 }
