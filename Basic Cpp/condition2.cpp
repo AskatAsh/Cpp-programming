85f57c4f7167da47a8efa198b6cadcdb5bbf969d
AskatAsh <abdulaskat@gmail.com>
2023-08-09 02:25:37 +0600

    more about condition with char datatype

---
+++ Basic Cpp/condition2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+        cout<<ch<<" is a vowel"<<endl;
+    }
+    else{
+        cout<<ch<<" is a consonant"<<endl;
+    }
+}
