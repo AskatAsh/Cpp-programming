65dae87ad73f09d317f179ed88f88400d5aad394
AskatAsh <abdulaskat@gmail.com>
2023-08-09 02:43:38 +0600

    learned about for loop break and continue

--- Basic Cpp/loops.cpp
+++ Basic Cpp/loops.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main(){
     int i,n;
     cin>>n;
-    for(i=1;i<=n;i++){
+    // for(i=1;i<=n;i+=2){
+    //     cout<<i<<" ";
+    // }
+    for(i=2;i<=n;i+=2){
+        // if(i==8)break;
+        if(i==4)continue;
         cout<<i<<" ";
+        // if(i==4)break;
     }
 }
