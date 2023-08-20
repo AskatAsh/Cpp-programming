80bd0a005575c143c6fade3b64f5d4bc4a00b849
AskatAsh <abdulaskat@gmail.com>
2023-08-20 19:10:10 +0600

    learned dynamic allocation using cpp keywords

--- Basic Cpp/dynamic-allocation.cpp
+++ Basic Cpp/dynamic-allocation.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main(){
     //int *p = (int*)malloc(10*sizeof(int));
-    int *p = (int*)calloc(10,sizeof(int));
+    //int *p = (int*)calloc(10,sizeof(int));
+    int *p = new int[10]{23, 12, 10, 27, 32};
 
     for(int i=0;i<10;i++){
         cout<<p[i]<<endl;
     }
-    free(p);
+    //free(p);
+    delete p;
 }
