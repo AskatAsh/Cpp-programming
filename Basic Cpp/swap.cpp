08fe4d8f99a4622d5d820427e4d06ea441c94d98
AskatAsh <abdulaskat@gmail.com>
2023-08-22 17:32:30 +0600

    swaping two numbers using swap library function

--- Basic Cpp/swap.cpp
+++ Basic Cpp/swap.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
+#include<utility>
 using namespace std;
-void swap(int *a, int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
+// void swap(int *a, int *b){
+//     int temp;
+//     temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
 int main()
 {
     int x,y;
     cin>>x>>y;
-    swap(&x, &y);
+    // swap(&x, &y);
+    swap(x,y);
     cout<<x<<" "<<y<<endl;
 }
