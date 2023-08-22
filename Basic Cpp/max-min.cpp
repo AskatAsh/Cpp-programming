dc8aa5f9c4c346710d3656d3aee218e1fc174702
AskatAsh <abdulaskat@gmail.com>
2023-08-22 17:13:06 +0600

    max and min using cpp library function

--- Basic Cpp/max-min.cpp
+++ Basic Cpp/max-min.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int my_max(int a,int b){
-    if(a>b) return a;
-    else return b;
-}
-int my_min(int a,int b){
-    if(a<b) return a;
-    else return b;
-}
+// int my_max(int a,int b){
+//     if(a>b) return a;
+//     else return b;
+// }
+// int my_min(int a,int b){
+//     if(a<b) return a;
+//     else return b;
+// }
 int main()
 {
     int x,y;
     cin>>x>>y;
-    int max = my_max(x,y);
-    cout<<"Max is : "<<max<<endl;
-    int min = my_min(x,y);
-    cout<<"Min is : "<<min<<endl;
+    // int max = my_max(x,y);
+    int maximum = max(x,y);
+    cout<<"Max is : "<<maximum<<endl;
+    int minimum = min(x,y);
+    cout<<"Min is : "<<minimum<<endl;
 }
