eef1b2f943d6b8abe541bdda16ae4d653d916f30
AskatAsh <abdulaskat@gmail.com>
2023-09-08 00:48:33 +0600

    used for loop to swap an array

---
+++ Basic Cpp/swap2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num[]={2,4,1,5,6};
+    int n = 5;
+
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(num[i]>num[j]){
+                int temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+    return 0;
+}
