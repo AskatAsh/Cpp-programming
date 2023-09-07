cd4f897a549faa209bbd4b9cb4c3b0e98f600db5
AskatAsh <abdulaskat@gmail.com>
2023-09-08 01:07:05 +0600

    used min max function for multiple variables

--- Basic Cpp/max-min2.cpp
+++ Basic Cpp/max-min2.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
-
+/*
 int minimum(int a, int b){
     if(a<b)return a;
     else return b;
-}
+}*/
 int main()
 {
-    int a,b;
-    a = 17, b = 50;
-    int mx = max(a,b);
+    int a=10,b=20,c=40,d=17;
+    //a = 17, b = 50;
+    //int mx = max(a,b);
+    //int mn = minimum(a,b);
 
-    int mn = minimum(a,b);
-    cout<<mx<<endl;
-    cout<<mn<<endl;
+    int mn = min(a,min(b,min(c,d)));
+    int mx = max(a,max(b,max(c,d)));
+    cout<<"max = "<<mx<<endl;
+    cout<<"min = "<<mn<<endl;
     /**
     if(a>b){
         cout<<a<<endl;
