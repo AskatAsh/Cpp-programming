1ec2c05f93f10a0b183ce5c0f05656b9d027b303
AskatAsh <abdulaskat@gmail.com>
2023-09-09 22:18:35 +0600

    solved atcoder problem B-break-number

---
+++ Basic Cpp/maximum-power-of-3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    int i=1;
+    cin>>n;
+    while(i*3<=n)
+    {
+        i = i*3;
+    }
+    cout<<i<<endl;
+}
---
+++ Problem Solving/B-Break-Number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i*2<=n){
+        i = i*2;
+    }
+    cout<<i<<endl;
+}
