99b9a77408d511cb2564e0dc6e1d88c12c2ca724
AskatAsh <abdulaskat@gmail.com>
2023-09-11 00:09:23 +0600

    added cpp program sum of diagonal a matrix

---
+++ Basic Cpp/diagonal-matrix-sum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num[3][3]=
+    {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int sum = 0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i == j){
+                sum = sum + num[i][j];
+            }
+        }
+    }
+    cout<<"Sum of Diagonal of a matrix is: "<<sum<<endl;
+}
