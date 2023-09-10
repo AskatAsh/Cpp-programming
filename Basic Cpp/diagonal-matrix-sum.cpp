f9ee9c88bfdf3b7bb9faf0879309e039e82f927c
AskatAsh <abdulaskat@gmail.com>
2023-09-11 00:16:15 +0600

    changed code in sum of diagonal of a matrix

--- Basic Cpp/diagonal-matrix-sum.cpp
+++ Basic Cpp/diagonal-matrix-sum.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main()
 {
-    int num[3][3]=
-    {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
-    int sum = 0;
+    int row,col,i,j,sum = 0;
+    cout<<"enter number of row and col: ";
+    cin>>row>>col;
+    int num[row][col];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>num[i][j];
+        }
+    }
+
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             if(i == j){
