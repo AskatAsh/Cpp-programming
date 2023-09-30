165bba7ddd653344a2d7f34652414f3913e482bd
AskatAsh <abdulaskat@gmail.com>
2023-10-01 02:25:28 +0600

    same letter in column pattern

---
+++ Pattern/pattern-10.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+
+        // for(int j=i;j>0;j--){
+        //     cout<<j<<" ";
+        // }
+        for(int j=1;j<=n;j++){
+            // cout<<char(i+64)<<" ";
+            char ch = 'A'+j-1;
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+
+    }
+}
+
+// output
+// A A A A 
+// B B B B 
+// C C C C 
+// D D D D 
