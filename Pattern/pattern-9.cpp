894a1a916f919add24bdf2debf750b82c82d30cf
AskatAsh <abdulaskat@gmail.com>
2023-10-01 02:14:49 +0600

    square pattern using alphabet

---
+++ Pattern/pattern-9.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+
+        // for(int j=i;j>0;j--){
+        //     cout<<j<<" ";
+        // }
+        for(int j=1;j<=n;j++){
+            cout<<char(i+64)<<" ";
+        }
+        cout<<endl;
+
+    }
+}
+
+// output
+// 1 
+// 2 3 
+// 3 4 5 
+// 4 5 6 7 
