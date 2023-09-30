70d1e47a5dcb63373b3e853ab4336ec88286d85c
AskatAsh <abdulaskat@gmail.com>
2023-10-01 02:01:01 +0600

    solving same reverse pattern differently

--- Pattern/pattern-8.cpp
+++ Pattern/pattern-8.cpp
@@ -5,8 +5,11 @@ int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
 
-        for(int j=i;j>0;j--){
-            cout<<j<<" ";
+        // for(int j=i;j>0;j--){
+        //     cout<<j<<" ";
+        // }
+        for(int j=1;j<=i;j++){
+            cout<<i-j+1<<" ";
         }
         cout<<endl;
 
