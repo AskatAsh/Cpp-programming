8514d1ab95141ab12e5e5d81f6b547d820001c1d
AskatAsh <abdulaskat@gmail.com>
2023-10-01 14:43:18 +0600

    alphabetic pattern in reverse by row and column

---
+++ Pattern/pattern-17.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   
+    for(int i=1;i<=n;i++){
+        // char x = 'A'+i-1;
+        for(int j=1;j<=i;j++){
+            // int x = n-i+j;
+            char x = 'A'+n-i+j-1;
+            cout<<x<<" ";
+            // x++;
+        }
+        cout<<endl;
+    }
+}
+
+// output:
+// D 
+// C D 
+// B C D 
+// A B C D 
+
+// 4
+// 3 4
+// 2 3 4
+// 1 2 3 4
