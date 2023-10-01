061b5b6346509b9e1df88197061bc85b960b6bff
AskatAsh <abdulaskat@gmail.com>
2023-10-01 15:18:44 +0600

    solved lower triangle matrix star pattern

---
+++ Pattern/pattern-20.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<i;j++){
+            cout<<"  ";
+        }
+        for(int j=n-i+1;j>=1;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+// output:
+// * * * *
+// _ * * *
+// _ _ * *
+// _ _ _ *
---
+++ Pattern/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for(int j=n-i;j>=i;j--){
+            cout<<"* ";
+        }
