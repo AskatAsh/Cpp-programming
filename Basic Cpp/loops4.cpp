59bd3c48ee1ab51781a91f93919b780f42245acc
AskatAsh <abdulaskat@gmail.com>
2023-10-02 15:55:36 +0600

    used nested for loop

--- Basic Cpp/loops4.cpp
+++ Basic Cpp/loops4.cpp
@@ -1,11 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    for(int i=0; i<=15; i+=2){
-        cout<<i<<" ";
-        if(i&1){
-            continue;
+    // for(int i=0; i<=15; i+=2){
+    //     cout<<i<<" ";
+    //     if(i&1){
+    //         continue;
+    //     }
+    //     i++;
+    // }
+
+    for(int i=0;i<5;i++){
+        for(int j=i;j<=5;j++){
+            cout<<i<<" "<<j<<endl;
         }
-        i++;
     }
 }
