92b4ddbc45ccbcea77f661d7819d21f32ea686bd
AskatAsh <abdulaskat@gmail.com>
2023-10-06 23:31:49 +0600

    solved leetcode problem power of two

---
+++ LeetCode/power-of-two.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<32;i++){
+        if(n == pow(2,i)){
+            cout<<true<<endl;
+            break;
+        }
+    }
+}
