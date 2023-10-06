f43b64b8244f9b8620a5398d9d0f6d2f37d17c09
AskatAsh <abdulaskat@gmail.com>
2023-10-06 23:45:21 +0600

    solved power-of-two using another method

--- LeetCode/power-of-two.cpp
+++ LeetCode/power-of-two.cpp
@@ -4,10 +4,20 @@ int main(){
     int n;
     cin>>n;
 
-    for(int i=0;i<32;i++){
-        if(n == pow(2,i)){
+    // for(int i=0;i<32;i++){
+    //     if(n == pow(2,i)){
+    //         cout<<true<<endl;
+    //         break;
+    //     }
+    // }
+    int ans = 1;
+    for(int i=0;i<31;i++){
+        if(n == ans){
             cout<<true<<endl;
             break;
+        }
+        if(ans<INT_MAX/2){
+            ans = ans * 2;
         }
     }
 }
