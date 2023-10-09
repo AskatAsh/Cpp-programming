4b6e7ca30fb782bbfd5640b3182790fe071ce946
AskatAsh <abdulaskat@gmail.com>
2023-10-09 21:44:10 +0600

    used switch to count notes to make a certain amount

--- Basic Cpp/money-counting.cpp
+++ Basic Cpp/money-counting.cpp
@@ -3,32 +3,43 @@ using namespace std;
 int main(){
     int amount,note,count;
     cin>>amount;
-    cin>>note;
+    // cin>>note;
 
-    switch(note){
+    switch(100){
         case 100:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/100;
+            cout<<count<<" notes of 100 = "<<count*100<<endl;
+            amount = amount%100;
+            // break;
         case 50:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/50;
+            cout<<count<<" notes of 50 = "<<count*50<<endl;
+            amount = amount%50;
+            // break;
         case 20:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/20;
+            cout<<count<<" notes of 20 = "<<count*20<<endl;
+            amount = amount%20;
+            // break;
         case 10:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/10;
+            cout<<count<<" notes of 10 = "<<count*10<<endl;
+            amount = amount%10;
+            // break;
         case 5:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/5;
+            cout<<count<<" notes of 5 = "<<count*5<<endl;
+            amount = amount%5;
+            // break;
+        case 2:
+            count = amount/2;
+            cout<<count<<" notes of 2 = "<<count*2<<endl;
+            amount = amount%1;
+            // break;
         case 1:
-            count = amount/note;
-            cout<<count<<" notes of "<<note<<endl;
-            break;
+            count = amount/1;
+            cout<<count<<" notes of 1 = "<<count*1<<endl;
+            amount = amount%1;
+            // break;
     }
 }
