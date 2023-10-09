8eb7e9f2319338a23c91fe9d420e2a9c7d24a20f
AskatAsh <abdulaskat@gmail.com>
2023-10-09 20:58:43 +0600

    used switch case with return 0 to break infinite loop

---
+++ Basic Cpp/switch-while.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    while(1){
+        int choice;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                cout<<"You choose option 1"<<endl;
+                break;
+            case 2:
+                cout<<"You choose option 2"<<endl;
+                break;
+            case 3:
+                cout<<"You choose option 3. exit program"<<endl;
+                return 0;
+            default:
+                cout<<"Invalid choice. Try again"<<endl;
+        }
+        
+    }
+}
