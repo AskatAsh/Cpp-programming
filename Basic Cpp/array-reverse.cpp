3363839557f993cf4b4e3a4fa74e5a3c0aafe99e
AskatAsh <abdulaskat@gmail.com>
2023-10-15 17:26:13 +0600

    used swap function to reverse the array

--- Basic Cpp/array-reverse.cpp
+++ Basic Cpp/array-reverse.cpp
@@ -12,11 +12,11 @@ void reverse(int arr[], int size){
 
     int start = 0;
     int end = size-1;
-    while(start<end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
+    while(start <= end){
+        // int temp = arr[start];
+        // arr[start] = arr[end];
+        // arr[end] = temp;
+        swap(arr[start], arr[end]);
         start++;
         end--;
     }
