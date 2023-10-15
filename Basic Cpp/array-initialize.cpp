9eaa18215cb2bdc8fe7475e54215401d32251906
AskatAsh <abdulaskat@gmail.com>
2023-10-15 13:05:00 +0600

    initialize an array with any value using for loop

---
+++ Basic Cpp/array-initialize.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[100];
+    int value = 3;
+    // initialize an array with certain value
+    for(int i=0;i<100;i++){
+        arr[i] = value;
+    }
+
+    // print the elements of the array
+    for(int i=0;i<100;i++){
+        cout<<arr[i]<<" ";
+    }
+}
