fa88ef5c3a9192d8cfe344a4723fc32a01671036
AskatAsh <abdulaskat@gmail.com>
2023-10-15 17:21:46 +0600

    reversed array using different method

--- Basic Cpp/array-reverse.cpp
+++ Basic Cpp/array-reverse.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 void reverse(int arr[], int size){
-    int temp[size];
-    for(int i=0; i<size; i++){
-        temp[i] = arr[size-i-1];
+    // int temp[size];
+    // for(int i=0; i<size; i++){
+    //     temp[i] = arr[size-i-1];
+    // }
+    // for(int i=0; i<size; i++){
+    //     cout<<temp[i]<<" ";
+    // }
+
+    int start = 0;
+    int end = size-1;
+    while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
     }
+
     for(int i=0; i<size; i++){
-        cout<<temp[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 }
+
 int main(){
     int numbers[8] = {1, 3, 17, 5, 9, 16, 15, 24};
     int size = 8;
