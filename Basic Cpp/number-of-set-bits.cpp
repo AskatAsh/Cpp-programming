5ed9698c5288593328b17348153133cfc9fedc58
AskatAsh <abdulaskat@gmail.com>
2023-10-15 12:51:09 +0600

    improved number of set bits program

--- Basic Cpp/number-of-set-bits.cpp
+++ Basic Cpp/number-of-set-bits.cpp
@@ -1,21 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int setBits(int n1, int n2){
+int countSetBits(int num){
     int count = 0;
-    while(n1!=0){
-        if((n1&1) == 1){
-            count++;
-        }
-        n1 = n1>>1;
-    }
-    while(n2!=0){
-        if((n2&1) == 1){
-            count++;
-        }
-        n2 = n2>>1;
+    while(num!=0){
+        num = num & (num-1);
+        count++;
     }
     return count;
+}
+int setBits(int n1, int n2){
+    // int count = 0;
+    // while(n1!=0){
+    //     if((n1&1) == 1){
+    //         count++;
+    //     }
+    //     n1 = n1>>1;
+    // }
+    // while(n2!=0){
+    //     if((n2&1) == 1){
+    //         count++;
+    //     }
+    //     n2 = n2>>1;
+    // }
+    // return count;
+    return countSetBits(n1)+countSetBits(n2);
 }
 
 int main(){
