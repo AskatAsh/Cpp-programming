66b6dc0f5ae80b009859ff91fb54fc8a6640941f
AskatAsh <abdulaskat@gmail.com>
2023-10-16 23:03:41 +0600

    solved the same problem in different method

--- CodingNinja/Find-Unique.cpp
+++ CodingNinja/Find-Unique.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int findUnique(int *arr, int size)
 {
+    // for(int i=0; i<size; i++){
+    //     int count = 0;
+    //     for(int j=0; j<size; j++){
+    //         if(arr[i] == arr[j]){
+    //             count++;
+    //         }
+    //     }
+    //     if(count == 1){
+    //         return arr[i];
+    //     }
+    // }
+    // return -1;
+    int unique = 0;
     for(int i=0; i<size; i++){
-        int count = 0;
-        for(int j=0; j<size; j++){
-            if(arr[i] == arr[j]){
-                count++;
-            }
-        }
-        if(count == 1){
-            return arr[i];
-        }
+        unique = unique ^ arr[i];
     }
-    return -1;
+    return unique;
 }
 
 int main(){
