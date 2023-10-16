26f6d777248ea0c3796af376d5b87856e4b53d10
AskatAsh <abdulaskat@gmail.com>
2023-10-16 22:06:10 +0600

    solved swap alternate using for loop

--- Basic Cpp/swap-alternate.cpp
+++ Basic Cpp/swap-alternate.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 void swapAlternate(int arr[], int size){
-    int first = 0;
-    int second = 1;
-    while(first < size && second < size){
-        swap(arr[first], arr[second]);
-        first += 2;
-        second += 2;
+    // int first = 0;
+    // int second = 1;
+    // while(first < size && second < size){
+    //     swap(arr[first], arr[second]);
+    //     first += 2;
+    //     second += 2;
+    // }
+    for(int i=0; i<size; i+=2){
+        if(i+1 < size){
+            swap(arr[i], arr[i+1]);
+        }
     }
     for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
