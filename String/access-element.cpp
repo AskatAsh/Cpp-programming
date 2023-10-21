3d0bb633cac89967a79359d91547a6d4c9b82a7c
AskatAsh <abdulaskat@gmail.com>
2023-10-21 22:48:24 +0600

    used string front and back method

--- String/access-element.cpp
+++ String/access-element.cpp
@@ -5,5 +5,8 @@ int main(){
     str = "abcdefghij";
     // cout<<"char at index 4 "<<str[4]<<endl;
     cout<<"char at index 4 "<<str.at(4)<<endl;
-
+    // cout<<"first element "<<str[0]<<endl;
+    // cout<<"last element "<<str[str.size()-1]<<endl;
+    cout<<"first element "<<str.front()<<endl;
+    cout<<"last element "<<str.back()<<endl;
 }
