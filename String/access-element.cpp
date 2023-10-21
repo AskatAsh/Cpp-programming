484d47e1b6519025317aa36ace8c3584fc782dd4
AskatAsh <abdulaskat@gmail.com>
2023-10-21 22:45:02 +0600

    used string at method to get element

---
+++ String/access-element.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    str = "abcdefghij";
+    // cout<<"char at index 4 "<<str[4]<<endl;
+    cout<<"char at index 4 "<<str.at(4)<<endl;
+
+}
