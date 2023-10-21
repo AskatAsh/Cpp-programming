25a2c48126acf32fa52b74eb17ca09e83876f221
AskatAsh <abdulaskat@gmail.com>
2023-10-21 22:10:49 +0600

    compared two strings

---
+++ String/compare-string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str1, str2;
+    str1 = "abcd";
+    str2 = "xyz";
+    if(str1 == str2){
+        cout<<"strings are equal"<<endl;
+    }
+    else{
+        cout<<"strings are not equal"<<endl;
+    }
+}
