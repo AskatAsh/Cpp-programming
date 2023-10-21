ec4dc9ea9414f716f7e68e4e040fd5f75b4a70ec
AskatAsh <abdulaskat@gmail.com>
2023-10-21 22:07:26 +0600

    added one string to another

--- String/string-concat.cpp
+++ String/string-concat.cpp
@@ -4,6 +4,8 @@ int main(){
     string str1, str2;
     str1 = "abcd";
     str2 = "xyz";
-    str1 = str2;
-    cout<<str1<<endl;
+    // str1 = str2;
+    // cout<<str1<<endl;
+    string concate = str1+str2;
+    cout<<concate<<endl;
 }
