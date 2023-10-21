392baf5e30079877e69b09b968bc706978624b97
AskatAsh <abdulaskat@gmail.com>
2023-10-21 21:59:38 +0600

    used string.capacity

---
+++ String/string-capacity.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    str = "abc";
+    cout<<str.capacity()<<endl;
+    str = "hello I am askat.";
+    cout<<str.capacity()<<endl;
+}
