d82a751f37a9e5cd445a112f37da54eb1d49cf24
AskatAsh <abdulaskat@gmail.com>
2023-10-22 15:44:56 +0600

    used string pop_back and push_back method

--- String/string-modifiers.cpp
+++ String/string-modifiers.cpp
@@ -4,6 +4,8 @@ int main(){
     string str = "abcdef";
     string str2 = "xyz";
     // str.assign("xyz");
-    str.append(str2);
+    // str.append(str2);
+    str.pop_back();
+    str.push_back('p');
     cout<<str<<endl;
 }
