968d49c859fdb79414f2da885fa3d5e18cd5d3a3
AskatAsh <abdulaskat@gmail.com>
2023-10-22 18:31:17 +0600

    used find_first and last not_of

--- String/string-operations.cpp
+++ String/string-operations.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string str = "agcsddassda";
+    string str = "aaagcsddassda";
     // cout<<str.find('s')<<endl;
     // cout<<str.rfind('s')<<endl;
-    cout<<str.find_first_of('s')<<endl;
-    cout<<str.find_last_of('x')<<endl;
+    // cout<<str.find_first_of('s')<<endl;
+    // cout<<str.find_last_of('x')<<endl;
+    cout<<str.find_first_not_of('a')<<endl;
+    cout<<str.find_last_not_of('a');
 }
