6e97de5f81e104e1cd3c4d0d0ed1b747cfe29937
AskatAsh <abdulaskat@gmail.com>
2023-10-22 18:50:04 +0600

    used stringstream without constructor

--- String/string-stream.cpp
+++ String/string-stream.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
     string str = "Hello I am Askat";
-    stringstream stream(str);
+    // stringstream stream(str);
+    stringstream stream;
+    stream<<str;
     string word;
     while(stream>>word){
         cout<<word<<" "<<word.size()<<endl;
