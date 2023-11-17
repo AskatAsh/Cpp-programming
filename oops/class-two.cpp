1e6ddbbe8723e5eef980c55c8d16620b2bdb3136
AskatAsh <abdulaskat@gmail.com>
2023-11-17 15:48:16 +0600

    used public  access modifier and dot operator

--- oops/class-two.cpp
+++ oops/class-two.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
-#include "Hero.cpp"
+// #include "Hero.cpp"
 using namespace std;
 
 class Empty{
     // this is empty class
 };
-// class Hero{
-//     int health;
-//     char name;
-// };
+class Hero{
+    public:
+    int health;
+    char name;
+};
 class Health {
     int value;
 };
@@ -17,7 +18,12 @@ int main(){
     Health v1;
     Hero h1;
     Empty emp;
+
+    h1.health = 75;
+    h1.name = 'A';
     // cout<<"size : "<<sizeof(v1)<<endl;
     // cout<<"size : "<<sizeof(h1)<<endl;
-    cout<<"size : "<<sizeof(emp)<<endl;
+    // cout<<"size : "<<sizeof(emp)<<endl;
+    cout<<"health is : "<<h1.health<<endl;
+    cout<<"name is : "<<h1.name<<endl;
 }
