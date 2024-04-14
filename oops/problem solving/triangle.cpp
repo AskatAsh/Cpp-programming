2a649f6a598b128eb187690af5b47f7a015e2f73
AskatAsh <abdulaskat@gmail.com>
2024-04-14 20:36:13 +0600

    used class constructor and if else to find triangle type

---
+++ oops/problem solving/triangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Triangle{
+    public:
+    double sideA, sideB, sideC;
+
+    Triangle(double a, double b, double c): sideA(a), sideB(b), sideC(c){}
+
+    void triNature(){
+        if(sideA == sideB && sideB == sideC){
+            cout<<"The Triangle is equilateral."<<endl;
+        }else if(sideA == sideB || sideA == sideC || sideB == sideC){
+            cout<<"The Triangle is isosceles."<<endl;
+        }else{
+            cout<<"The Triangle is Scelene."<<endl;
+        }
+    }
+};
+
+int main(){
+    Triangle tri(12, 15, 19);
+    tri.triNature();
+}
