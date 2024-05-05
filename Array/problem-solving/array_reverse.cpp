cacda09a698a7906867f1c1a3be9b2a71ed5a373
AskatAsh <abdulaskat@gmail.com>
2024-05-05 20:33:18 +0600

    used reverse function to solve the problem

--- Array/problem-solving/array_reverse.cpp
+++ Array/problem-solving/array_reverse.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 void array_reverse(int array[], int size)
 {
-    int temp;
-    for (int i = 0; i < size / 2; i++)
-    {
-        temp = array[i];
-        array[i] = array[size - (i + 1)];
-        array[size - (i + 1)] = temp;
-    }
+    // int temp;
+    // for (int i = 0; i < size / 2; i++)
+    // {
+    //     temp = array[i];
+    //     array[i] = array[size - (i + 1)];
+    //     array[size - (i + 1)] = temp;
+    // }
+    reverse(array, array+size);
 }
 
 int main()
