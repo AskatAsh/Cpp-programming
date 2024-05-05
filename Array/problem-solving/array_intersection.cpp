a03b3dc708dad8d298b9f04155e1894b933f56aa
AskatAsh <abdulaskat@gmail.com>
2024-05-05 22:52:23 +0600

    used two pointer technique to solve the problem

--- Array/problem-solving/array_intersection.cpp
+++ Array/problem-solving/array_intersection.cpp
@@ -4,16 +4,30 @@ using namespace std;
 
 void array_intersection(int array_A[], int m, int array_B[], int n)
 {
-    int found = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(array_B[i] == array_A[j]){
-                cout<<array_B[i]<<" ";
-                found = 1;
-            }
+    // 1 2 3 4 5 6 7 8 -> m [i]
+    // 0 1 2 3 6 9     -> n [j]
+    bool found = false;
+    int i=0,j=0;
+    while(i<m && j<n){
+        if(array_A[i] == array_B[j]){
+            cout<<array_A[i]<<" ";
+            found = true;
+            i++; j++;
         }
+        else if(array_A[i] > array_B[j]) j++;
+        else i++;
     }
-    if(found == 0) cout<<"Empty Set"<<endl;
+    if(!found) cout<<"Empty Set"<<endl;
+    // int found = 0;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<m; j++){
+    //         if(array_B[i] == array_A[j]){
+    //             cout<<array_B[i]<<" ";
+    //             found = 1;
+    //         }
+    //     }
+    // }
+    // if(found == 0) cout<<"Empty Set"<<endl;
 }
 
 int main()
