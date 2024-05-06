8b5d67203cd0a6babe42573755f3a6f0b34380dc
AskatAsh <abdulaskat@gmail.com>
2024-05-07 00:13:41 +0600

    decreased iteration for bubble sort

--- Array/bubble_sort.cpp
+++ Array/bubble_sort.cpp
@@ -1,28 +1,38 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 void bubble_sort(int array[], int size)
 {
-    // 11 1  5  4  14
+    // 14 11 1  5  4
     // 0  1  2  3  4
-    for(int i=1; i<size; i++){
-        for(int j=0; j<size-1; j++){ // j = 3
-            if(array[j] < array[j+1])
+    for (int i = 1; i < size; i++) //i = 3
+    {
+        int count = 0;
+        for (int j = 0; j < size - 1; j++) // j = 0
+        {
+            if (array[j] > array[j + 1])
             {
-                swap(array[j], array[j+1]);
+                swap(array[j], array[j + 1]);
+            }
+            if(array[size-j-1] > array[size-j-2])
+            {
+                count++;
             }
         }
+        // 1 5 4 11 14
+        cout<<"loop count: "<<i<<endl;
+        if(count == size-1) break;
     }
 }
 
 void print_array(int array[], int size)
 {
-    for(int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<array[i]<<" ";
+        cout << array[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
@@ -41,6 +51,6 @@ int main()
 
     bubble_sort(array, size);
 
-    cout<<"After Using bubble sort:"<<endl;
+    cout << "After Using bubble sort:" << endl;
     print_array(array, size);
 }
