fb2e5b8b3304d3926cfbe578afbc00a19e33e317
AskatAsh <abdulaskat@gmail.com>
2024-05-07 22:00:53 +0600

    insert element in a sorted array

--- Array/problem-solving/insert_element.cpp
+++ Array/problem-solving/insert_element.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 void print_array(int array[], int size)
 {
-    for(int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<array[i]<<" ";
+        cout << array[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void insert_element(int array[], int size, int value, int pos)
+void insert_element(int array[], int size, int value)
 {
-    array[size] = array[pos-1];
-    array[pos-1] = value;
-    print_array(array, size);
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > value)
+        {
+            cout<<"Iteration count: "<<i<<endl;
+            for (int j = size - 1; j >= i; j--)
+            {
+                array[j + 1] = array[j];
+            }
+            array[i] = value;
+            break;
+        }
+    }
+    print_array(array, size+1);
 }
 
 int main()
@@ -23,17 +35,18 @@ int main()
     cout << "Enter array size: ";
 
     cin >> size;
-    int array[size+1];
+    int array[size + 1];
 
     cout << "Array input:" << endl;
     for (int i = 0; i < size; i++)
     {
         cin >> array[i];
     }
+    sort(array, array + size);
 
-    int value, pos;
-    cout << "Enter Value and Position: ";
-    cin >> value >> pos;
+    int value;
+    cout << "Enter Value to insert: ";
+    cin >> value;
 
-    insert_element(array, size, value, pos);
+    insert_element(array, size, value);
 }
